98af06a39c774a83d702644263f4b1444844c1ac
Matt <mkschleg@umail.iu.edu>
2019-03-24 17:55:13 -0600

    Adding deps directory to hopefully fix issues with python gym install...

--- deps/build.jl
+++ deps/build.jl
@@ -13,8 +13,8 @@ function install_python()
             println("PyCall Error -- assuming module not found")
             println("Gym not installed. Installing now.")
             # run(`$(joinpath(Conda.python_dir(Conda.ROOTENV),"python")) -c 'print("Hello")'`)
-            run(`ls $(Conda.python_dir(Conda.ROOTENV))`)
-            # run(`$(joinpath(Conda.python_dir(Conda.ROOTENV),"pip")) install 'gym[all]'`)
+            # run(`ls $(Conda.python_dir(Conda.ROOTENV))`)
+            run(`$(joinpath(Conda.python_dir(Conda.ROOTENV),"python")) -m pip install 'gym[all]'`)
             # py"""import pip; pip install 'gym[all]';"""
             copy!(pygym, pyimport("gym"))
         else
