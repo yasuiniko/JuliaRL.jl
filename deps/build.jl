72518e5cdd47649669084ba71a0b42d0bd79b8d3
Matt <mkschleg@umail.iu.edu>
2019-03-24 17:26:40 -0600

    Adding deps directory to hopefully fix issues with python gym install...

--- deps/build.jl
+++ deps/build.jl
@@ -1,28 +1,28 @@
 using PyCall
+using Conda
 
+# run(`$(joinpath(Conda.python_dir(Conda.ROOTENV),"python")) -m pip install 'gym[all]'`)
 
-run(`$(joinpath(Conda.python_dir(Conda.ROOTENV),"python")) -m pip install 'gym[all]'`)
-
-# function install_python()
-#     try
-#         copy!(pygym, pyimport("gym"))
-#     catch ex
-#         println("Error in Gym Module")
-#         println(ex)
-#         if isa(ex, PyCall.PyError)
-#             if ex.T.__name__ == "ModuleNotFoundError"
-#                 println("Gym not installed. Installing now.")
-#                 # run(`$(joinpath(Conda.python_dir(Conda.ROOTENV),"python")) -c 'print("Hello")'`)
+function install_python()
+    try
+        copy!(pygym, pyimport("gym"))
+    catch ex
+        println("Error in Gym Module")
+        println(ex)
+        if isa(ex, PyCall.PyError)
+            if ex.T.__name__ == "ModuleNotFoundError"
+                println("Gym not installed. Installing now.")
+                # run(`$(joinpath(Conda.python_dir(Conda.ROOTENV),"python")) -c 'print("Hello")'`)
                 
-#                 # py"""import pip; pip install 'gym[all]';"""
-#                 copy!(pygym, pyimport("gym"))
-#             else
-#                 throw(ex)
-#             end
-#         else
-#             throw(ex)
-#         end
-#     end
-# end
+                # py"""import pip; pip install 'gym[all]';"""
+                copy!(pygym, pyimport("gym"))
+            else
+                throw(ex)
+            end
+        else
+            throw(ex)
+        end
+    end
+end
 
-# install_python()
+install_python()
